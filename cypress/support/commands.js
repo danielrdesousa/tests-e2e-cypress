828e4c5ce53b0ecb1d057d69ff176c5800f61417
Daniel Rodrigues <danielrdesousa@gmail.com>
2022-06-19 13:00:25 -0300

    feat: tests logout and config CI/CD

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -36,6 +36,26 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   cy.get('#confirmationCode').should('be.visible');
 });
 
+Cypress.Commands.add('fillSettingsFormAndSubmit', () => {
+  cy.visit('/settings');
+  cy.get('#storage').type('1');
+  cy.get('#name').type('Mary Doe');
+  cy.iframe('.card-field iframe')
+    .as('iframe')
+    .find('[name="cardnumber"]')
+    .type('4242424242424242');
+  cy.get('@iframe')
+    .find('[name="exp-date"]')
+    .type('1271');
+  cy.get('@iframe')
+    .find('[name="cvc"]')
+    .type('123');
+  cy.get('@iframe')
+    .find('[name="postal"]')
+    .type('12345');
+  cy.contains('button', 'Purchase').click();
+});
+
 Cypress.Commands.add('login',
   (
     username = Cypress.env('USER_EMAIL'),
